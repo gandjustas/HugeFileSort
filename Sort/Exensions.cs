dc1da360dfa3df3fc9b7fcd1e53add63f9247373
gandjustas <gandjustas@mail.ru>
2022-09-07 16:31:04 +0300

    Добалена реализация minheap вместо PriorityQueue

--- Sort/Exensions.cs
+++ Sort/Exensions.cs
@@ -9,35 +9,73 @@ namespace Sort
     public static class Exensions
     {
         public static IEnumerable<string> EnumerateLines(this StreamReader reader)
-        {            
+        {
             while (!reader.EndOfStream)
             {
                 yield return reader.ReadLine()!;
             }
-        }        
+        }
 
         public static IEnumerable<string> MergeLines(this IEnumerable<IEnumerable<string>> sources, IComparer<string> comparer)
         {
-            var enumerators = from source in sources
-                              let e = source.GetEnumerator()
-                              where e.MoveNext()
-                              select (e,e.Current);
-            var heap = new PriorityQueue<IEnumerator<string>, string>(enumerators, comparer);
+            var heap = (from source in sources
+                        let e = source.GetEnumerator()
+                        where e.MoveNext()
+                        select e).ToArray();
+            BuildHeap(heap, e => e.Current, comparer);
 
-            while (heap.Count > 0)
+            while (true)
             {
-                var min = heap.Dequeue();
+                var min = heap[0];
                 yield return min.Current;
-                if (min.MoveNext())
+                if (!min.MoveNext())
                 {
-                    heap.Enqueue(min, min.Current);
-                }
-                else
-                { 
                     min.Dispose();
+                    if (heap.Length == 1) break;
+                    heap[0] = heap[^1];
+                    Array.Resize(ref heap, heap.Length-1);
                 }
+                Heapify(heap, e => e.Current, 0, comparer);
             }
         }
+
+        private static void Heapify<T, V>(T[] heap, Func<T, V> selector, int index, IComparer<V> comparer)
+        {
+            var min = index;
+            while (true)
+            {
+                var leftChild = 2 * index + 1;
+                var rightChild = 2 * index + 2;
+                var v = selector(heap[index]);
+
+                if (rightChild < heap.Length && comparer.Compare(v, selector(heap[rightChild])) > 0)
+                {
+                    min = rightChild;
+                }
+
+                if (leftChild < heap.Length && comparer.Compare(v, selector(heap[leftChild])) > 0)
+                {
+                    min = leftChild;
+                }
+
+                if (min == index) break;
+
+                var temp = heap[index];
+                heap[index] = heap[min];
+                heap[min] = temp;
+                
+                index = min;
+            }
+        }
+
+        private static void BuildHeap<T, V>(T[] heap, Func<T, V> selector, IComparer<V> comparer)
+        {
+            for (int i = heap.Length / 2; i >= 0; i--)
+            {
+                Heapify(heap, selector, i, comparer);
+            }
+        }
+
 
     }
 }
