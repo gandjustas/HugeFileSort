e0f3130e20145b4fcc5622cde5ba3f7fc32545e6
gandjustas <gandjustas@mail.ru>
2023-01-25 02:27:32 +0300

    Реализовали слияние с помощью кучи

--- Sort/Exensions.cs
+++ Sort/Exensions.cs
@@ -2,24 +2,79 @@
 {
     public static class Exensions
     {
-        public static IEnumerable<T> Merge<T>(this IEnumerable<IEnumerable<T>> sources, IComparer<T> comparer = default)
+public static void Heapify<T>(this Span<T> heap, int index, IComparer<T> comparer)
+{
+    ArgumentNullException.ThrowIfNull(comparer);
+
+    var min = index;
+    while (true)
+    {
+        var leftChild = 2 * index + 1;
+        var rightChild = 2 * index + 2;
+        var v = heap[index];
+
+        if (rightChild < heap.Length && comparer.Compare(v, heap[rightChild]) > 0)
         {
-            var enumerators = (from source in sources
-                               let e = source.GetEnumerator()
-                               where e.MoveNext()
-                               select e).ToList();
-            
-            while (enumerators.Count > 0)
-            {
-                var min = enumerators.MinBy(e => e.Current, comparer)!;
-                yield return min.Current;
-                if (!min.MoveNext())
-                {
-                    min.Dispose();
-                    enumerators.Remove(min);
-                }
-            }
+            min = rightChild;
+            v = heap[min];
         }
 
+        if (leftChild < heap.Length && comparer.Compare(v, heap[leftChild]) > 0)
+        {
+            min = leftChild;
+        }
+
+        if (min == index) break;
+
+        var temp = heap[index];
+        heap[index] = heap[min];
+        heap[min] = temp;
+
+        index = min;
+    }
+}
+
+public static void BuildHeap<T>(this Span<T> heap, IComparer<T> comparer)
+{
+    ArgumentNullException.ThrowIfNull(comparer);
+
+    for (int i = heap.Length / 2; i >= 0; i--)
+    {
+        Heapify(heap, i, comparer);
+    }
+}
+
+public static IEnumerable<T> Merge<T>(this IEnumerable<IEnumerable<T>> sources, IComparer<T> comparer = default)
+{
+    var heap = (from source in sources
+                let e = source.GetEnumerator()
+                where e.MoveNext()
+                select e).ToArray();
+
+    var enumeratorComparer = new EnumeratorComparer<T>(comparer ?? Comparer<T>.Default);
+    heap.AsSpan().BuildHeap(enumeratorComparer);
+
+    while (true)
+    {
+        var min = heap[0];
+        yield return min.Current;
+        if (!min.MoveNext())
+        {
+            min.Dispose();
+            if (heap.Length == 1) yield break;
+            heap[0] = heap[^1];
+            Array.Resize(ref heap, heap.Length - 1);
+        }
+        heap.AsSpan().Heapify(0, enumeratorComparer);
+    }
+}
+
+private record EnumeratorComparer<T>(IComparer<T> comparer) : IComparer<IEnumerator<T>>
+{
+    public int Compare(IEnumerator<T>? x, IEnumerator<T>? y)
+    {
+        return comparer.Compare(x!.Current, y!.Current);
+    }
+}
     }
 }
--- Sort/Program.cs
+++ Sort/Program.cs
@@ -13,14 +13,14 @@ var sw = System.Diagnostics.Stopwatch.StartNew();
 
 var count = 0;
 var tempFiles =
-    File.ReadLines(file)
-        .Select(s => (s, s.IndexOf('.')))
-        .Chunk(chunkSize)
-        .Select(chunk =>
+    File.ReadLines(file)                    // Читаем построчно
+        .Select(s => (s, s.IndexOf('.')))   // Находим точку
+        .Chunk(chunkSize)                   // Разбиваем на куски по 1М строк
+        .Select(chunk =>                    
         {
-            Array.Sort(chunk, comparer);
+            Array.Sort(chunk, comparer);    // Каждый кусок сортируем
             var tempFileName = Path.ChangeExtension(file, $".part-{count++}" + Path.GetExtension(file));
-            File.WriteAllLines(tempFileName, chunk.Select(x => x.Item1));
+            File.WriteAllLines(tempFileName, chunk.Select(x => x.Item1)); // Сохраняем отсортированные строки в файл
             return tempFileName;
         }).ToList();
 
@@ -30,9 +30,9 @@ sw.Restart();
 try
 {
     var mergedLines = tempFiles
-        .Select(f => File.ReadLines(f).Select(s => (s, s.IndexOf('.'))))
-        .Merge(comparer)
-        .Select(x => x.Item1);
+        .Select(f => File.ReadLines(f).Select(s => (s, s.IndexOf('.')))) // Читаем построчно все файлы, находим в строках точку
+        .Merge(comparer)  //Слияние итераторов IEnumerable<IEnumerable<(string,int)>> в IEnumerable<(string,int)>
+        .Select(x => x.Item1); // Оставляем только строки
     File.WriteAllLines(Path.ChangeExtension(file, ".sorted" + Path.GetExtension(file)), mergedLines);
 }
 finally
