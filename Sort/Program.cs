a95221c9d1703f08a6fe00a5eecba0d3dd13bf69
gandjustas <gandjustas@mail.ru>
2023-01-28 12:36:55 +0300

    Добавлено распараллеливание

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Buffers;
 using System.Diagnostics;
 using System.Text;
-
+using System.Threading.Channels;
+using WorkItem = System.Collections.Generic.List<(System.ReadOnlyMemory<char>, int)>;
 
 internal class Program : IDisposable
 {
@@ -12,43 +13,79 @@ internal class Program : IDisposable
     private int chunkSize;
     private StringComparison currentCulture;
     Encoding detectedEncoding;
+    int degreeOfParallelism;
     Comparer comparer;
     List<string> tempFiles = new();
     Random random = new();
+    Channel<WorkItem> readToSort;
+    Channel<WorkItem> sortToWrite;
 
 
     System.Diagnostics.Stopwatch timer = new();
 
 
-    public Program(string file, int chunkSize, StringComparison currentCulture)
+    public Program(string file, int chunkSize, StringComparison currentCulture, int degreeOfParallelism)
     {
         this.file = file;
         this.chunkSize = chunkSize;
         this.currentCulture = currentCulture;
+        this.degreeOfParallelism = degreeOfParallelism;
         this.comparer = new Comparer(StringComparison.CurrentCulture);
+
+        readToSort = Channel.CreateBounded<WorkItem>(1);
+        sortToWrite = Channel.CreateBounded<WorkItem>(1);
+
     }
     public void Dispose()
     {
         tempFiles.ForEach(File.Delete);
     }
 
-    public void SplitSort()
+    public async Task SplitSort()
     {
         timer.Restart();
 
-        using var reader = new StreamReader(file, Encoding.Default, true, BufferSize);
+        SemaphoreSlim semaphore = new(1);
+
+        var sorterThreads = Enumerable.Range(0, degreeOfParallelism)
+            .Select(_ => Task.Run(async () =>
+            {
+                await foreach (var chunk in readToSort.Reader.ReadAllAsync())
+                {
+                    chunk.Sort(comparer);
+                    await sortToWrite.Writer.WriteAsync(chunk);
+                }
+                // Не можем так сделать, когда сортеров будет много
+                //sortToWrite.Writer.Complete();
+            }))
+            .ToArray();
+
+        var writerThread = Task.Run(async () =>
+        {
+            await foreach (var chunk in sortToWrite.Reader.ReadAllAsync())
+            {
+                await semaphore.WaitAsync();
+                //Записываем чанки на диск
+                await WriteChunk(chunk);
+                semaphore.Release();
+            }
+        });
+
+        using var reader = new StreamReader(file, Encoding.UTF8, true, BufferSize);
         detectedEncoding = reader.CurrentEncoding;
 
-        List<(ReadOnlyMemory<char>, int)> chunk = new();
         var chunkBuffer = new char[chunkSize];
         var chunkReadPosition = 0;
         while (true)
         {
-            var charsRead = reader.ReadBlock(chunkBuffer, chunkReadPosition, chunkSize - chunkReadPosition);
+            await semaphore.WaitAsync();
+            int charsRead = await reader.ReadBlockAsync(chunkBuffer, chunkReadPosition, chunkSize - chunkReadPosition);
+            semaphore.Release();
             var eos = reader.EndOfStream;
             var m = chunkBuffer.AsMemory(0, chunkReadPosition + charsRead);
 
             // Заполняем список строк ReadOnlyMemory<char> для сортировки
+            WorkItem chunk = new();
             int linePos;
             while ((linePos = m.Span.IndexOf(Environment.NewLine)) >= 0)
             {
@@ -63,20 +100,24 @@ internal class Program : IDisposable
                 chunk.Add((m, m.Span.IndexOf('.')));
             }
 
-            chunk.Sort(comparer);
+            await readToSort.Writer.WriteAsync(chunk);
+            //chunk.Sort(comparer);
 
-            //Записываем чанки на диск
-            WriteChunk(chunk);
 
             if (eos) break;
-            chunk.Clear();
-
-            //Отсток буфера переносим в начало
+            //Остаток буфера переносим в начало нового массива
+            chunkBuffer = new char[chunkSize];
             m.CopyTo(chunkBuffer);
             chunkReadPosition = m.Length;
         }
+        readToSort.Writer.Complete();
+        await Task.WhenAll(sorterThreads);
+        sortToWrite.Writer.Complete();
+        await writerThread;
         Console.WriteLine($"SplitSort done in {timer.Elapsed}");
     }
+
+
 
     public void Merge()
     {
@@ -95,9 +136,7 @@ internal class Program : IDisposable
         Console.WriteLine($"Merge done in {timer.Elapsed}");
     }
 
-
-
-    void WriteChunk(List<(ReadOnlyMemory<char>, int)> chunk)
+    async Task WriteChunk(WorkItem chunk)
     {
         // Записываем строки из отсортированного списка во временный файл
         var tempFileName = Path.ChangeExtension(file, $".part-{tempFiles.Count}" + Path.GetExtension(file));
@@ -107,13 +146,14 @@ internal class Program : IDisposable
 
             foreach (var (l, _) in chunk)
             {
-                tempFile.WriteLine(l);
+                await tempFile.WriteLineAsync(l);
             }
+            await tempFile.FlushAsync();
         }
         tempFiles.Add(tempFileName);
     }
 
-    private static int Main(string[] args)
+    private static async Task<int> Main(string[] args)
     {
         if ((args?.Length ?? 0) == 0)
         {
@@ -122,10 +162,10 @@ internal class Program : IDisposable
         }
 
         var file = args![0];
-        var chunkSize = args?.Length > 1 ? int.Parse(args[1]) * 1000_000 / 2 : 100_000_000; //In characters
+        var chunkSize = args?.Length > 1 ? int.Parse(args[1]) * 1000_000 / 2 : 10_000_000; //In characters
 
-        using var app = new Program(file, chunkSize, StringComparison.CurrentCulture);
-        app.SplitSort();
+        using var app = new Program(file, chunkSize, StringComparison.CurrentCulture, 2);
+        await app.SplitSort();
         app.Merge();
 
         return 0;
